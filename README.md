44361c944ef2d825988eb56f5e99bd33daada429
raghunadh2006ster <raghunadh2006ster@me.com>
2021-02-20 12:19:40 +0530

    splash screen done with icons and themes

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # TheClassroom
+
+Todo: 
+
+Splash Screen - done completely.
+Login Screen - icons - backgrounds - phone fields - login
+Contacts Screen - self name, contact names - mvvm arch
+Outgoing Call Screen - 
+Incoming Call Screen - 
+Call Screen - floating action buttons for switch cam, end call
+
---
+++ app/src/main/java/com/brigosha/theclassroom/SplashScreenPackage/SplashActivity.java
@@ -0,0 +1,35 @@
+package com.brigosha.theclassroom.SplashScreenPackage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.brigosha.theclassroom.MainActivity;
+
+import static java.lang.Math.atan;
+import static java.lang.Math.tan;
+
+public class SplashActivity extends AppCompatActivity {
+    /*  Set theme from manifest file.
+        Theme is saved inside styles.xml
+        Icons from appIcon.co
+        drawable/splash.xml is used as theme instead of layoutInflater for faster loading
+        Check v<24 compatibility
+     */
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        //simulate heavy operations
+
+//        for (int i =0; i<=1000000;i++) {
+//            long d = (long) tan(atan(tan(atan(tan(atan(tan(atan(tan(atan(123456789.123456789))))))))));
+//            System.out.println(d);
+//        }
+        startActivity(new Intent(this, MainActivity.class));
+        finish();
+    }
+}
