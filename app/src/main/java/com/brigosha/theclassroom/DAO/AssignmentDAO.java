55940430fa30df9b772ad292eaef1506500b7a11
AkhiJix <jichkarakhilesh@gmail.com>
2021-04-06 20:08:00 +0530

    assignment dao and db update

--- app/src/main/java/com/brigosha/theclassroom/DAO/AssignmentDAO.java
+++ app/src/main/java/com/brigosha/theclassroom/DAO/AssignmentDAO.java
@@ -1,7 +1,29 @@
 package com.brigosha.theclassroom.DAO;
 
+import static androidx.room.OnConflictStrategy.REPLACE;
+
+import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import com.brigosha.theclassroom.Models.Assignment;
 
 @Dao
-public class AssignmentDAO {
+public interface AssignmentDAO {
+
+    @Query("SELECT * FROM assignments")
+    LiveData<Assignment> getAssignments();
+
+    @Insert
+    void insertAssignment(Assignment... assignments);
+
+    @Delete
+    void deleteAssignment(Assignment... assignments);
+
+    @Update(onConflict = REPLACE)
+    void updateAssignment(Assignment... assignments);
+
 }
--- app/src/main/java/com/brigosha/theclassroom/Database/AssignmentDatabase.java
+++ app/src/main/java/com/brigosha/theclassroom/Database/AssignmentDatabase.java
@@ -1,12 +1,28 @@
 package com.brigosha.theclassroom.Database;
 
+import android.content.Context;
+
 import androidx.room.Database;
+import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
 import com.brigosha.theclassroom.DAO.AssignmentDAO;
 import com.brigosha.theclassroom.Models.Assignment;
 
 @Database(entities = {Assignment.class}, version = 1)
-abstract class AssignmentDatabase extends RoomDatabase {
-//    abstract public AssignmentDAO
+public abstract class AssignmentDatabase extends RoomDatabase {
+
+    private static final String DATABASE_NAME = "CLASSROOM-DB";
+    private static AssignmentDatabase assignmentDbInstance;
+
+    public abstract AssignmentDAO asgDao();
+    public static AssignmentDatabase getAssignmentDao(Context context){
+        if(assignmentDbInstance == null){
+            assignmentDbInstance = Room.databaseBuilder(context,AssignmentDatabase.class,DATABASE_NAME)
+                    .fallbackToDestructiveMigration()
+                    .build();
+        }
+        return assignmentDbInstance;
+    }
+
 }
